aa8a7d959de95e780ecca76dcbe6a953e9e164ba
Vadim <krpnv@mail.ru>
2023-04-03 19:44:19 +0100

    added react and updated branding

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import { SquadifyTextIcon } from './icons/SquadifyTextIcon'
+
+const App = () => {
+    return (
+        <div className="App">
+            <SquadifyTextIcon width="80%" />
+        </div>
+    )
+}
+
+export default App
